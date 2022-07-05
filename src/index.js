6269b33084561539d937d1be44083456b9b53a86
Daniel Vega Egea <d.vega.egea@gmail.com>
2022-07-05 11:58:20 +0200

    Rearranging elements.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import App from './app/App';
+import reportWebVitals from './tools/reportWebVitals';
+import './css/index.css';
 //import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
