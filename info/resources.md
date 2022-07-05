6477679bffd1315b4684e271f4a9b59563f03979
Daniel Vega Egea <d.vega.egea@gmail.com>
2022-07-05 16:05:39 +0200

    Adding React-Markdown and it works.

--- info/resources.md
+++ info/resources.md
@@ -7,4 +7,8 @@
 - [React Router](https://v5.reactrouter.com/web/guides/quick-start)
 - [React Helmet](https://www.npmjs.com/package/react-helmet)
 - [Styled Components](https://styled-components.com/)
+- [React-Markdown](https://www.npmjs.com/package/react-markdown)
+
+## Discarded
+
 - [MDX-Js - Markdown for the Component Era](https://mdxjs.com/)
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,20 @@
 //import HomepageText from '../assets/markdown/HomepageText.mdx';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+const markdownText = `
+# Presentación
+Esto es una página de prueba para ver si todo funciona.
+Veamos un texto en **negrita** y otro en _cursiva_.
+
+## Hello, world!
+Esto es un segundo párrafo.
+`;
 
 const Homepage = () => {
   return (
     <>
-      <div>
-        {/* <HomepageText /> */}
-        Text
-      </div>
+      <ReactMarkdown children={markdownText} remarkPlugins={[remarkGfm]} />
     </>
   );
 };
