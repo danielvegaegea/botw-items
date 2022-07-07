309684ec4c4a20287f89596912905feac3dd5dd6
Daniel Vega Egea <d.vega.egea@gmail.com>
2022-07-07 20:47:35 +0200

    Low minimum operation.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 
 // Pages
 import Homepage from '../pages/Homepage';
-import CounterPage from '../pages/CounterPage';
+/* import CounterPage from '../pages/CounterPage'; */
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/counter-page" element={<CounterPage />} />
+      {/* <Route path="/counter-page" element={<CounterPage />} /> */}
     </Routes>
   );
 };
---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import { createRoot } from 'react-dom/client';
+/* import { Provider } from 'react-redux'; */
+/* import { store } from './app/store'; */
+import App from './app/App';
+/* import reportWebVitals from './tools/reportWebVitals'; */
+import './css/index.css';
+//import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+/* const container = document.getElementById('root');
+const root = createRoot(container); */
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    {/* <Provider store={store}> */}
+    <Router>
+      <App />
+    </Router>
+    {/* </Provider> */}
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+/* reportWebVitals(); */
